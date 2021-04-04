45ceb9313adca62c7312fb317be8dcb5f80c672d
Aaron Barack <atbarack@gmail.com>
2021-04-04 15:43:52 +0900

    Created working login and logout functionality and readded active players on refresh

--- index.js
+++ index.js
@@ -4,16 +4,50 @@ const fs = require("fs");
 const process = require("process");
 const cors = require("cors");
 const axios = require("axios");
+const crypto = require('crypto');
 
 const app = express();
 const port = 3000;
 const api = "https://api.triviabeat.io";
+const aaron = true;
+
+const algorithm = 'aes-256-ctr';
+const secretKey = 'w4A0gFdUr3tG0VtHDHJhaVFm2pTMwV1b';
+const iv = crypto.randomBytes(16);
+
+const encrypt = (text) => {
+
+    const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+
+    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
+
+    return {
+        iv: iv.toString('hex'),
+        content: encrypted.toString('hex')
+
+    };
+};
+
+const decrypt = (hash) => {
+    //console.log(hash)
+    const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(hash.iv, 'hex'));
+
+    const decrpyted = Buffer.concat([decipher.update(Buffer.from(hash.content, 'hex')), decipher.final()]);
+
+    return decrpyted.toString();
+};
+
+
+
+
+
+
 
 process.on("uncaughtException", function (err) {
   console.error(err && err.stack ? err.stack : err);
 });
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+if(!aaron) process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 // if log folder exists
 if (fs.existsSync("/var/log/triviabeatui")) {
@@ -24,41 +58,78 @@ if (fs.existsSync("/var/log/triviabeatui")) {
   process.stderr.write = serr.write.bind(serr);
 }
 
+
+
 app.use(express.json({ limit: "1mb" }));
-app.use(cors());
+var corsOptions = {
+    origin: '*',
+    'Access-Control-Allow-Origin': '*',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+
+app.use(cors(corsOptions));
 
 app.use("/", express.static("public"));
 app.use("/lib", express.static("public/lib"));
 
-app.put("/login", (req, res) => {
-  axios({
-    method: "PUT",
-    url: api + "/session",
-    timeout: 1000 * 10, // Wait for 10 seconds
-    data: {
-      email: req.headers.email,
-      password: req.headers.password,
-    },
-  })
-    .then((response) => {
-      console.log("----- Response Success -----");
-      console.log(response.status);
-      console.log(response.data);
-      console.log("----------------------------");
-      res.write(response.data);
-    })
-    .catch((error) => {
-      //this will say if a timeout happened
-      console.log(error.code)
-      console.log(error.message)
-      console.log(error.stack)
 
-      console.log("----- Response Failed -----");
-      console.log(error.response.status);
-      console.log("---------------------------");
-      res.write(error.response.status);
-    });
+app.get("/login", (req, res, next) =>{
+  res.sendFile('public/login.html', {root: __dirname })
+})
+app.put("/login",cors(), (req, res, next) => {
+
+  var yes = {
+    'email': req.headers.email,
+    'password': req.headers.password
+  }
+  JSON.stringify(yes);
+  var exit;
+  axios.put(
+      "https://api.triviabeat.io/session",
+        yes,
+        {
+        timeout: 5000,
+        json: true,
+        headers: {
+              'Content-Type': 'application/json',
+            }
+       }).then( function (response){
+      const hash = encrypt(JSON.stringify(response.data));
+      res.json(hash)
+      res.end();
+    })
+    .catch(function(error) {
+        if(error !== undefined){
+          console.log("----- Response Failed -----");
+          console.log(error);
+          console.log("---------------------------");
+          res.end();
+        }
+    })
+
+
 });
+
+
+
+app.put("/verify",cors(), (req, res, next) => {
+
+
+  //console.log(req.headers.data)
+  const result = decrypt(JSON.parse(req.headers.data))
+    console.log(result)
+    var date = new Date();
+    if(result.expiration < date.getTime()){
+      res.sendStatus(400)
+    }
+    res.end();
+
+
+
+});
+
+
+
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -87,17 +158,14 @@ io.sockets.on(
     console.log("We have a new client: " + socket.id);
     clients++;
     // When this user emits, client side: socket.emit('otherevent',some data);
-
-    socket.on("mouse", function (data) {
+    socket.broadcast.emit("player", clients);
+    socket.on("player", function (data) {
       // Data comes in as whatever was sent, including objects
-      console.log("Received: 'mouse' " + data.x + " " + data.y);
-      io.sockets.emit("mouse", data);
+      io.sockets.emit("player", clients);
 
-      // This is a way to send to everyone including sender
-      // io.sockets.emit('message', "this goes to everyone");
     });
     //socket.on("clients", function(data){
-    io.sockets.emit("clients", clients);
+    //io.sockets.emit("clients", clients);
     //})
     socket.on("disconnect", function () {
       console.log("Client has disconnected");

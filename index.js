f3ccf8a32485ce4562ac46aeb75154887750c857
Felix Doppelhofer <philipp.doppelhofer@outlook.at>
2021-04-07 21:38:49 +0000

    server listen to any ip addr

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.use((req, res, next) => {
   next();
 });
 
-var server = app.listen(port, "127.0.0.1", () => {
+var server = app.listen(port, () => {
   console.log(`Trivia Beat app listening at port ${port}`);
 });
 

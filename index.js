980a6dbf903b0d123cdb07306b7b3a330ef6451b
Aaron Barack <atbarack@gmail.com>
2021-05-10 21:49:08 +0900

    reactful

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const globalSTS = sts.getSTS({'max-age':{'days': 30}});
 
 app.use(globalSTS);
 app.use(express.json({ limit: "1mb" }));
-app.use("/", express.static("public/triviabeat-react/build"));
+app.use("/", express.static("public/react/build"));
 
 app.get("/question", (req, res) => {
   res.header({

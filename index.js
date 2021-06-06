43aa269d428543c903718e62dd0a1f31c9c897d2
Aaron Barack <atbarack@gmail.com>
2021-06-06 11:36:43 +0900

    Basic register and login works! (:

--- index.js
+++ index.js
@@ -58,11 +58,18 @@ const globalSTS = sts.getSTS({'max-age':{'days': 30}});
 app.use(globalSTS);
 app.use(express.json({ limit: "1mb" }));
 app.use("/", express.static("public/old"));
-
+app.use("/static", express.static("public/react/build/static"));
 
 app.get("/", (req, res) =>{
-  if(req.cookies.login == undefined) return res.status(200).sendFile("public/old/login.html", { root: __dirname });
-  else return res.sendFile("public/react/build", { root: __dirname })
+  console.log(req.cookies)
+
+  //var session = res.cookies.login
+  db.query(format("select id from player where session_id = '%s'", req.cookies.login), (err,result) =>{
+    if(result.rowCount == 1) return res.sendFile("public/react/build/index.html", { root: __dirname })
+    else return res.sendFile("public/old/login.html" , { root: __dirname })
+  })
+
+
 })
 
 app.get("/:file", (req, res) =>{
@@ -78,16 +85,34 @@ app.put("/login" , (req, res) => {
   //params needed: email, password
   var date = new Date();
   db.query(format("select id from player where email = '%s' AND password = '%s' limit 1", req.headers.email, req.headers.password), (err, result) =>{
-
-     res.status(200).cookie('login', uuidv4(), {expire: date.getDate() + 30, secure: false, sameSite: true, httpOnly: true }).send(err || result.rowCount == 1 ? true: false);
+      const session = uuidv4();
+      //need to the uuid to the player now that they have logged in
+      if(result.rowCount == 1) {
+        db.query(format("update player set session_id = '%s' where id = '%s'", session, result.rows[0].id ), (err1, result1) =>{
+          res.cookie('login', session, {
+            maxAge: (60*60*24*30),
+            httpOnly: true,
+            secure: false
+           })
+          //console.log(result.rows[0].id);
+          res.status(200).send(err || result.rowCount == 1 ? true: false);
+          //res.status(303).re
+        })
+      }
   })
 });
 
 app.put("/register", (req, res) => {
   //params needed username, email, password
-
-  db.query(format("insert into player (username, email, password, session_id) values ('%s', '%s', '%s', '%s'", req.headers.username, req.headers.email, req.headers.password), (err, result)=>{
-
+  const session = uuidv4();
+  db.query(format("insert into player (username, email, password, session_id) values ('%s', '%s', '%s', '%s')", req.headers.username, req.headers.email, req.headers.password, session), (err, result)=>{
+    console.log(err || result)
+    res.cookie('login', session, {
+      maxAge: (60*60*24*30),
+      httpOnly: true,
+      secure: false
+     })
+    res.status(200).redirect('/')
   })
 
 });
@@ -95,16 +120,16 @@ app.put("/register", (req, res) => {
 
 app.put("/username", (req, res) => { //check username
   //params needed, username
-  db.query(format("select id from player where username = '%s' limit 1", req.headers.username), (err, result) =>{
+  db.query(format("select username from player where username = '%s' limit 1", req.headers.username), (err, result) =>{
     //console.log(err || result);
     return res.status(200).send(err || result.rowCount == 0 ? true: false);
   })
 });
 
 app.put("/email", (req, res) => {
-  db.query(format("select id from player where email = %s limit 1", req.headers.username), (err, result) =>{
+  db.query(format("select email from player where email = '%s' limit 1", req.headers.email), (err, result) =>{
     //console.log(err || result);
-    return res.status(200).send(err || result.rows ? true: false);
+    return res.status(200).send(err || result.rows == 0 ? true: false);
   })
 });
 
